49cad724df1e8078b4d88297a7bdb0227d3a1b64
SKVDIO <skadio1990@gmail.com>
2024-01-21 21:16:14 +0200

    jenkins

--- tests/utils/logger.ts
+++ tests/utils/logger.ts
@@ -3,18 +3,7 @@ import {
     TestCase,
     TestError,
     TestResult,
-    TestStep,
-    Suite,
-    JSONReport,
-    JSONReportSuite,
-    JSONReportSpec,
-    JSONReportTest,
-    JSONReportTestResult,
-    JSONReportError,
-    JSONReportTestStep,
-    JSONReportSTDIOEntry,
-    Location
-
+    TestStep
 } from "@playwright/test/reporter";
 import winston from "winston";
 
@@ -42,45 +31,39 @@ const logger = winston.createLogger({
 });
 
 export default class CustomReporter implements Reporter {
-    // onTestBegin(test: TestCase): void {
-    //     logger.info(`Test started : ${test.title}`);
-    // }
+    onTestBegin(test: TestCase): void {
+        logger.info(`Test started : ${test.title}`);
+    }
 
-//     // onStepBegin(test: TestCase, result: TestResult, step: TestStep): void {
-//     //     if (step.category === "test.step") {
-//     //         logger.info(`Step started : ${step.title}`);
-//     //     }
-//     // }
+    onStepBegin(test: TestCase, result: TestResult, step: TestStep): void {
+        if (step.category === "test.step") {
+            logger.info(`Step started : ${step.title}`);
+        }
+    }
 
-//     // onTestEnd(test: TestCase, result: TestResult): void {
-//     //     logger[result.status === "passed" ? "info" : "error"](
-//     //         `Test ${result.status} after ${result.duration}ms : ${test.title}`
-//     //     );
-//     // }
+    onTestEnd(test: TestCase, result: TestResult): void {
+        logger[result.status === "passed" ? "info" : "error"](
+            `Test ${result.status} after ${result.duration}ms : ${test.title}`
+        );
+    }
 
-//     // onTestEnd(test: TestCase, result: TestResult): void {
-//     //     logger.error(`Test ${result.status} after ${result.duration}ms : ${test.title}`);
-//     // }
+    onStdOut(
+        chunk: string | Buffer,
+        test: void | TestCase,
+        result: void | TestResult
+    ): void {
+        logger.debug(`${test?.title} : ${chunk}`);
+    }
 
-//     onStdOut(
-//         chunk: string | Buffer,
-//         test: void | TestCase,
-//         result: void | TestResult
-//     ): void {
-//         logger.debug(`${test?.title} : ${chunk}`);
-//     }
-
-//     // onStdErr(
-//     //     chunk: string | Buffer,
-//     //     test: void | TestCase,
-//     //     result: void | TestResult
-//     // ): void {
-//     //     logger.error(`${test?.title} : ${chunk}`);
-//     // }
+    onStdErr(
+        chunk: string | Buffer,
+        test: void | TestCase,
+        result: void | TestResult
+    ): void {
+        logger.error(`${test?.title} : ${chunk}`);
+    }
 
     onError(error: TestError): void {
         logger.error(error.message);
     }
-
-
 }
