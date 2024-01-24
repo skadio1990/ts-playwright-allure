1d346e2207fad8883bc7bba4b80d15d10535eab6
SKVDIO <skadio1990@gmail.com>
2024-01-24 17:55:52 +0200

    prometheus-grafana

--- tests/api/allele.spec.ts
+++ tests/api/allele.spec.ts
@@ -3,24 +3,24 @@ import { expect } from "../fixtures/expect";
 import { report } from "../utils/report";
 
 test("Assert Data Integrity", async ({ request }) => {
-  report({
-    suite: "API",
-    subSuite: "Functional",
-    description:
-      'Verify successful execution of a GET request to the \
+    report({
+        suite: "API",
+        subSuite: "Functional",
+        description:
+            'Verify successful execution of a GET request to the \
         "allele" endpoint, ensuring that the response status code is 200 \
         and subsequently validating the integrity of the API data returned.',
-    severity: "blocker",
-    link: ["https://api.cpicpgx.org", "Documentation"]
-  });
+        severity: "blocker",
+        link: ["https://api.cpicpgx.org", "Documentation"],
+    });
 
-  const response = await request.get("https://data.cpicpgx.org/v1/allele", {
-    params: {
-      limit: 1,
-    },
-  });
+    const response = await request.get("https://data.cpicpgx.org/v1/allele", {
+        params: {
+            limit: 1,
+        },
+    });
 
-  const exampleJson = "./tests/api/allele.json";
-  
-  await expect(response).toBeStructuredLike(exampleJson);
+    const exampleJson = "./tests/api/allele.json";
+
+    await expect(response).toBeStructuredLike(exampleJson);
 });
